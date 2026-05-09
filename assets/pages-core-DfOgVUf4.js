d870fa09e34e6e4489ffad9aa0bc45943703177a
Prashanth G L <guledalprashanth@gmail.com>
2026-05-09 17:04:21 +0200

    Updates

--- assets/pages-core-DfOgVUf4.js
+++ assets/pages-core-DfOgVUf4.js
@@ -82,7 +82,7 @@ dracut -f -v 2>&1 | head -50
 
 # Rebuild initramfs for ALL installed kernels at once
 for kv in $(ls /lib/modules/); do
-  dracut -f /boot/initramfs-${kv}.img ${kv}
+  dracut -f /boot/initramfs-\${kv}.img \${kv}
 done`}),e.jsx(n,{language:"bash",title:"dracut configuration files",code:`# System-wide dracut configuration
 cat /etc/dracut.conf
 
